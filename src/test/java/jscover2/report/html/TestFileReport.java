22a189da777263515f7ea0bc70ccad5271b66d49
tntim96 <tntim96@gmail.com>
2014-09-10 20:14:03 +1000

    Add test infrastructure

---
+++ src/test/java/jscover2/report/html/TestFileReport.java
@@ -0,0 +1,12 @@
+package jscover2.report.html;
+
+import jscover2.report.FileData;
+import jscover2.report.FileDataBuilder;
+import org.junit.Test;
+
+public class TestFileReport {
+    @Test
+    public void should() {
+        FileData fileData = new FileDataBuilder().build();
+    }
+}
