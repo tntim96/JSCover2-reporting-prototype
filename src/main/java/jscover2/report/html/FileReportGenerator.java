71be17230832d9bbc7e2f52e3627ea93958f0fee
tntim96 <tntim96@gmail.com>
2014-09-11 20:52:08 +1000

    Add basic file report

---
+++ src/main/java/jscover2/report/html/FileReportGenerator.java
@@ -0,0 +1,95 @@
+package jscover2.report.html;
+
+import jscover2.report.FileData;
+import jscover2.report.LineCompleteData;
+import jscover2.report.SourceCodeRetriever;
+import org.apache.commons.lang3.StringEscapeUtils;
+
+import static java.lang.String.format;
+
+public class FileReportGenerator {
+    private String code;
+    private FileData data;
+    private final SourceCodeRetriever sourceCodeRetriever;
+
+    public FileReportGenerator(String fileName, String code, FileData data) {
+        sourceCodeRetriever = new SourceCodeRetriever(fileName, code);
+        this.code = code;
+        this.data = data;
+    }
+
+    public String generateHtml() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<html>");
+        buildHeader(sb);
+        buildBody(sb);
+        sb.append("</html>");
+        return sb.toString();
+    }
+
+    private void buildHeader(StringBuilder sb) {
+        sb.append("<head>\n" +
+                "    <title>JSCover2 Coverage Report</title>\n" +
+                "    <style>\n" +
+                "        body {\n" +
+                "            font-size: 10pt;\n" +
+                "            font-family: monospace;\n" +
+                "        }\n" +
+                "        .line {\n" +
+                "            display:block;\n" +
+                "            text-align: right;\n" +
+                "        }\n" +
+                "        .hit {\n" +
+                "            background-color: greenyellow;\n" +
+                "        }\n" +
+                "        .miss {\n" +
+                "            background-color: orangered;\n" +
+                "        }\n" +
+                "    </style>\n" +
+                "</head>\n");
+    }
+
+    private void buildBody(StringBuilder sb) {
+        sb.append("<body>\n" +
+                "<table>\n" +
+                "    <tr>\n" +
+                "        <td><pre>");
+        addLines(sb);
+        sb.append("</pre></td>\n" +
+                "        <td valign=\"top\">");
+        addHits(sb);
+        sb.append("</td>\n" +
+                "        <td>\n" +
+                "<pre>");
+        addSource(sb);
+        sb.append("</pre>\n" +
+                "        </td>\n" +
+                "    </tr>\n" +
+                "</table>\n" +
+                "</body>\n");
+    }
+
+    private void addLines(StringBuilder sb) {
+        for (int i = 1; i <= this.sourceCodeRetriever.getNumberOfLines(); i++) {
+            sb.append(i);
+            sb.append('\n');
+        }
+    }
+
+    private void addHits(StringBuilder sb) {
+        int currentLine = 0;
+        for (Integer line : data.getLineData().keySet()) {
+            while (++currentLine < line) {
+                sb.append("<span class=\"line\">&nbsp;</span>\n");
+            }
+            LineCompleteData lineCompleteData = data.getLineData().get(line);
+            int lineHits = lineCompleteData.getLineHits();
+            String hitClass = lineCompleteData.hit() ? "hit" : "miss";
+            sb.append(format("<span class=\"line %s\">%d</span>\n", hitClass, lineHits));
+        }
+    }
+
+    private void addSource(StringBuilder sb) {
+        sb.append(StringEscapeUtils.escapeHtml4(code));
+    }
+}
--- src/test/java/jscover2/report/html/FileReportGeneratorTest.java
+++ src/test/java/jscover2/report/html/FileReportGeneratorTest.java
@@ -3,9 +3,7 @@ package jscover2.report.html;
 import jdk.nashorn.api.scripting.ScriptObjectMirror;
 import jscover2.instrument.Configuration;
 import jscover2.instrument.Instrumenter;
-import jscover2.report.CoverageData;
 import jscover2.report.FileData;
-import jscover2.report.FileDataBuilder;
 import jscover2.report.JSCover2Data;
 import org.apache.commons.io.FileUtils;
 import org.junit.Before;
@@ -21,27 +19,33 @@ import java.io.File;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-public class TestFileReport {
+public class FileReportGeneratorTest {
     private ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
     private Invocable invocable = (Invocable) engine;
     private Configuration config = new Configuration();
     private Instrumenter instrumenter;
+    private String code;
 
     @Before
     public void before() throws Exception {
         config.setCoverVariableName("jscover");
         instrumenter = new Instrumenter(config);
-        String code = FileUtils.readFileToString(new File("src/test/resources/script.js"));
+        code = FileUtils.readFileToString(new File("src/test/resources/script.js"));
         String instrumented = instrumenter.instrument("test.js", code);
         engine.eval(instrumented);
     }
 
     @Test
-    public void should() throws ScriptException, NoSuchMethodException {
+    public void shouldGenerateHtml() throws ScriptException, NoSuchMethodException {
         assertThat(invocable.invokeFunction("check", 1), equalTo("one or two"));
         assertThat(invocable.invokeFunction("check", 4), equalTo("four"));
 
         JSCover2Data jsCover2Data = new JSCover2Data((ScriptObjectMirror) engine.eval(config.getCoverVariableName()));
-        assertThat(jsCover2Data.getDataMap().get("test.js").getStatements().size(), equalTo(13));
+        FileData fileData = jsCover2Data.getDataMap().get("test.js");
+        assertThat(fileData.getStatements().size(), equalTo(13));
+
+        FileReportGenerator fileReportGeneratorReport = new FileReportGenerator("test.js", code, fileData);
+        String html = fileReportGeneratorReport.generateHtml();
+        System.out.println(html);
     }
 }
