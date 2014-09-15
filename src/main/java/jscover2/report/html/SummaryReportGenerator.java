fb5dc363d32a2fdae5f5230543f9e001b29e7066
tntim96 <tntim96@gmail.com>
2014-09-16 07:53:10 +1000

    Add hyperlink to file reports

--- src/main/java/jscover2/report/html/SummaryReportGenerator.java
+++ src/main/java/jscover2/report/html/SummaryReportGenerator.java
@@ -70,7 +70,7 @@ public class SummaryReportGenerator {
         for (CoverageSummaryData summaryData : summary.getFiles()) {
             sb.append("<tr>\n");
             sb.append("<td class=\"name\">");
-            sb.append(format("<a href=\"%s\">%s</a>", summaryData.getName().replaceAll("\\.js", ".html"), summaryData.getName()));
+            sb.append(format("<a href=\"%s.html\">%s</a>", summaryData.getName(), summaryData.getName()));
             sb.append("</td>\n");
             sb.append("<td class=\"graph\">");
             sb.append(format("<div class=\"coveredBackground\"><div class=\"covered\" style=\"width:%3.0fpx;\"></div></div>", summaryData.getLineCoverage().getRatio()*100));
